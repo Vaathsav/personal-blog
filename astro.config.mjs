b94adaaa3bbf4a261af5be0b01cfcc1963ce958c
Vaathsav <srivaathsav02@gmail.com>
2026-08-02 14:38:30 +0530

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://vaathsav.github.io',
+  base: '/personal-blog',
   integrations: [sitemap(), mdx()]
 });
